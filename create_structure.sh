9be895fa79d4a55858dda1470636acaa0c69f546
Sumit Kamra <sumitkamra@gmail.com>
2024-03-09 17:37:39 +0700

    First commit with the structure

---
+++ create_structure.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+mkdir notebooks expression_coding expression_coding/interface expression_coding/ml_logic tests
+mkdir expression_coding/app
+
+touch Makefile
+touch requirements.txt
+touch setup.py
+
+touch expression_coding/__init__.py
+
+touch expression_coding/interface/__init__.py
+touch expression_coding/interface/main_local.py
+
+touch expression_coding/app/app.py
+
+
+touch expression_coding/ml_logic/__init__.py
+touch expression_coding/ml_logic/data.py
+touch expression_coding/ml_logic/model.py
+touch expression_coding/ml_logic/preprocessor.py
+touch expression_coding/ml_logic/registry.py
+
+touch expression_coding/utils.py
+touch expression_coding/params.py
+touch .gitignore
+
+touch tests/__init__.py
